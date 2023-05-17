2481ff4a40dc7cfa6a38083397007b91ebbf66b8
suraji <surajidk12@gmail.com>
2023-05-17 11:23:54 +0700

    fix task today

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -5,17 +6,24 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "./App.css";
-import { Box, Button, TextField } from "@mui/material";
+import { Box, Button, Stack, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export default function SimpleAccordion() {
-  const token = "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";
+  const token =
+    "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adk";
 
   const [user, setUser] = useState<any>([]);
+  const [erro, setErro] = useState<any>();
+  const [detail, setDetail] = useState<any>([]);
+  const [search, setSearch] = useState({
+    input: "",
+    data: [],
+  });
 
-  useEffect(() => {
+  const getData = (url: string, setState: any) => {
     axios
-      .get("https://api.github.com/users", {
+      .get(url, {
         headers: {
           Accept: "application/vnd.github+json",
           Authorization: `Bearer ${token}`,
@@ -23,64 +31,135 @@ export default function SimpleAccordion() {
         },
       })
       .then((response) => {
-        console.log(response);
-
-        setUser(response);
+        setState(response);
       })
       .catch((error) => {
-        // handle error
-        console.log(error);
+        setErro(error);
       });
+  };
+
+  useEffect(() => {
+    getData("https://api.github.com/users", setUser);
   }, []);
+
+  const onHandleSearch = () => {
+    const filteredUser = user?.data?.filter((item: any) =>
+      item.login.toLowerCase().includes(search.input.toLowerCase())
+    );
+    setSearch({ ...search, data: filteredUser });
+  };
 
   return (
     <Box maxWidth="sm" mx={"auto"}>
       <TextField
         fullWidth
-        sx={{ background: "white", marginBottom: 2 }}
-        id="standard-basic"
-        label="Enter username"
-        variant="standard"
+        hiddenLabel
+        sx={{ background: "white", marginBottom: 1 }}
+        id="filled-hidden-label-small"
+        placeholder="Enter username"
+        variant="filled"
+        size="small"
+        onChange={(e) => setSearch({ ...search, input: e.target.value })}
       />
-      <Button fullWidth variant="contained" sx={{ marginBottom: 2 }}>
-        Contained
+      <Button
+        fullWidth
+        variant="contained"
+        sx={{ marginBottom: 1 }}
+        onClick={onHandleSearch}
+      >
+        Search
       </Button>
-      {user?.data?.map((item: any, idx: number) => {
-        return (
-          <Accordion key={idx}>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography>{item.login}</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget. */}
-                {item.repos_url}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        );
-      })}
-      {/* <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Accordion 2</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion> */}
+      {erro?.response?.status == 401
+        ? erro?.message
+        : search?.data?.length === 0
+        ? user?.data?.map((item: any, idx: number) => {
+            return (
+              <Accordion key={idx}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                  onClick={() => getData(item.repos_url, setDetail)}
+                >
+                  <Typography>{item.login}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {detail?.data?.map((item: any, idx: number) => {
+                      return (
+                        <Stack
+                          direction="row"
+                          justifyContent="space-between"
+                          alignItems="flex-start"
+                          spacing={2}
+                          key={idx}
+                          sx={{
+                            border: "1px solid gray",
+                            marginBottom: "8px",
+                            padding: "8px",
+                            background: "gray",
+                            borderRadius: "8px",
+                          }}
+                        >
+                          <Box sx={{ textAlign: "left" }}>
+                            <Box sx={{ fontWeight: 600 }}>{item.name}</Box>
+                            <Box sx={{ fontSize: 12 }}>{item.description}</Box>
+                          </Box>
+                          <Box sx={{ width: "30px", fontWeight: 600 }}>
+                            {item.forks_count} *
+                          </Box>
+                        </Stack>
+                      );
+                    })}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })
+        : search?.data?.map((item: any, idx: number) => {
+            return (
+              <Accordion key={idx}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                  onClick={() => getData(item.repos_url)}
+                >
+                  <Typography>{item.login}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Typography>
+                    {detail?.data?.map((item: any, idx: number) => {
+                      return (
+                        <Stack
+                          direction="row"
+                          justifyContent="space-between"
+                          alignItems="flex-start"
+                          spacing={2}
+                          key={idx}
+                          sx={{
+                            border: "1px solid gray",
+                            marginBottom: "8px",
+                            padding: "8px",
+                            background: "gray",
+                            borderRadius: "8px",
+                          }}
+                        >
+                          <Box sx={{ textAlign: "left" }}>
+                            <Box sx={{ fontWeight: 600 }}>{item.name}</Box>
+                            <Box sx={{ fontSize: 12 }}>{item.description}</Box>
+                          </Box>
+                          <Box sx={{ width: "30px", fontWeight: 600 }}>
+                            {item.forks_count} *
+                          </Box>
+                        </Stack>
+                      );
+                    })}
+                  </Typography>
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
     </Box>
   );
 }
